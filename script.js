08f9f329c386cbb4d04d39b505eccd0f6e1c3511
Jose Martinez <josemanuel21302@gmail.com>
2024-02-12 17:12:25 -0800

    function will now only show loading and vice versa

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+const quoteContainer = document.getElementById('quote-container')
 const newQuoteBtn = document.getElementById('newQuote')
 const quoteText = document.getElementById('quoteText')
 const author = document.querySelector('.author')
+const loader = document.getElementById('loader')
+
 // When page loads it will run the api once
 getQuotes()
 //stores the api values so that the page does not need to call the api multiple times
 let quotes = [];
 
+function loading(){
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+function loadingComplete(){
+    loader.hidden = true
+    quoteContainer.hidden = false
+}
 
 newQuoteBtn.addEventListener('click', newQuote)
 
