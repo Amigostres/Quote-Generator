1aa0860098a4763fba8a2a88c92c6192583148a1
Jose Martinez <josemanuel21302@gmail.com>
2024-02-12 16:58:31 -0800

    the api will no longer get called everytime the btn is pressed. we are storing the values onto a variable

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const newQuoteBtn = document.getElementById('newQuote');
+const newQuoteBtn = document.getElementById('newQuote')
 const quoteText = document.getElementById('quoteText')
 const author = document.querySelector('.author')
-//when page loads it will run the api once
-onload = getQuotes
-newQuoteBtn.addEventListener('click', getQuotes)
-function newQuote(quotes){
+// When page loads it will run the api once
+getQuotes()
+//stores the api values so that the page does not need to call the api multiple times
+let quotes = [];
+
+
+newQuoteBtn.addEventListener('click', newQuote)
+
+function newQuote(){
     //grab a random quote from array
     const newQuote = quotes[Math.floor(Math.random() * quotes.length)]
 
@@ -17,6 +22,6 @@ function newQuote(quotes){
 //get quotes from api
 async function getQuotes() {
     const response = await fetch("https://jacintodesign.github.io/quotes-api/data/quotes.json");
-    const quotes = await response.json();
-    newQuote(quotes)
+    quotes = await response.json();
+    newQuote()
   }
